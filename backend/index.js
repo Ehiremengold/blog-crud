66251073f77a817a9c6494e8b7812f92f0432bf3
Ehiremen <ehiremengold@gmail.com>
2025-11-17 23:25:10 +0100

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Included Vite logo and React logo SVGs in the public and assets directories.
    - Created main application component (App.tsx) and set up entry point (main.tsx).
    - Configured Tailwind CSS in index.css.
    - Defined TypeScript interfaces for Post in types.ts.
    - Set up TypeScript configuration files for app and node environments.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.ts.

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { connectDB } from "./config/db.js";
+import postsRouter from "./routes/post.js";
+import { config } from "dotenv";
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use("/api/posts", postsRouter);
+
+config();
+connectDB();
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ backend/routes/post.js
@@ -0,0 +1,65 @@
+import express from "express";
+const router = express.Router();
+import Post from "../models/Post.js";
+
+// Get all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get one post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Post - create post
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+  });
+  try {
+    const newPost = await post.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Put update post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!post) return res.status(404).json({ message: "Post not found" });
+    res.json(post);
+  } catch (error) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+    res.json({ message: "Post deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+export default router;
